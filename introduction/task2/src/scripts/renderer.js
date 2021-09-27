d09c867374aa086fd2bfb0ba6038e3aeb40a9e16
Denis <Deniss2007@gmail.com>
2021-09-27 16:20:23 +0300

    initial commit

---
+++ introduction/task2/src/scripts/renderer.js
@@ -0,0 +1,48 @@
+import { getItem } from './storage.js';
+
+const listElem = document.querySelector('.list');
+
+const compareTasks = (a, b) => {
+  if (a.done) {
+    return a.done - b.done || new Date(b.finishDate) - new Date(a.finishDate);
+  }
+  return new Date(b.createDate) - new Date(a.createDate);
+};
+
+const createCheckbox = ({ done, id }) => {
+  const checkboxElem = document.createElement('input');
+  checkboxElem.setAttribute('type', 'checkbox');
+  checkboxElem.setAttribute('data-id', id);
+  checkboxElem.checked = done;
+  checkboxElem.classList.add('list__item-checkbox');
+
+  return checkboxElem;
+};
+
+const createListItem = ({ text, done, id }) => {
+  const listItemElem = document.createElement('li');
+  listItemElem.classList.add('list__item');
+  const checkboxElem = createCheckbox({ done, id });
+
+  const textElem = document.createElement('span');
+  textElem.classList.add('list__item-text');
+  textElem.textContent = text;
+  if (done) {
+    textElem.classList.add('item_done');
+  }
+
+  const deleteBtnElem = document.createElement('button');
+  deleteBtnElem.classList.add('list__item-delete_btn');
+  listItemElem.append(checkboxElem, textElem, deleteBtnElem);
+
+  return listItemElem;
+};
+
+export const renderTasks = () => {
+  const tasksList = getItem('tasksList') || [];
+  console.log(tasksList);
+  listElem.innerHTML = '';
+  const tasksElems = tasksList.sort(compareTasks).map(createListItem);
+
+  listElem.append(...tasksElems);
+};
